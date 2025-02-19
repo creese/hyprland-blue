c30e518c2d8d6f8e9bda2d82e95a8a7f08aef799
Charles Reese <noreply@git>
2025-02-19 14:07:35 -0800

    Install Waybar and Font Awesome

--- build.sh
+++ build.sh
@@ -4,13 +4,15 @@ set -ouex pipefail
 
 ### Install packages
 
+rpm-ostree install waybar fontawesome-fonts-all
+
 # Packages can be installed from any enabled yum repo on the image.
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf install -y tmux 
+dnf install -y tmux
 
 # Use a COPR Example:
 #
